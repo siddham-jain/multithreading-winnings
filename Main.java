57c640286eb92171217e0716c48590c941c27181
siddham-jain <siddhamjain224@gmail.com>
2024-07-24 00:51:54 +0530

    Using different Threads

--- Main.java
+++ Main.java
@@ -1,5 +1,16 @@
 public class Main{
     public static void main(String[] args){
-        System.out.println("Hello World");
+        // System.out.println("Hello World");
+        HelloWorldPrinter t1 = new HelloWorldPrinter();
+        t1.start();
+        HelloWorldPrinter t2 = new HelloWorldPrinter();
+        t2.start();
+    }
+}
+
+class HelloWorldPrinter extends Thread{
+    @Override
+    public void run(){
+        System.out.println("Hello World " + Thread.currentThread().getName());
     }
 }

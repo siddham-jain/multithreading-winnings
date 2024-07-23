368af7038768577cc3ea1881bf711b129c184536
siddham-jain <siddhamjain224@gmail.com>
2024-07-24 01:21:02 +0530

    Priting numbers using two different threads

--- Main.java
+++ Main.java
@@ -2,9 +2,28 @@ public class Main{
     public static void main(String[] args){
         // System.out.println("Hello World");
         HelloWorldPrinter t1 = new HelloWorldPrinter();
-        t1.start();
+        // t1.start();
         HelloWorldPrinter t2 = new HelloWorldPrinter();
-        t2.start();
+        // t2.start();
+
+        NumberPrinter n1 = new NumberPrinter();
+        n1.start();
+        NumberPrinter n2 = new NumberPrinter();
+        n2.start();
+    }
+}
+
+class NumberPrinter extends Thread{
+    @Override
+    public void run(){
+        for(int i = 0; i < 10; i++){
+            try{
+                Thread.sleep(1000);
+            }catch(InterruptedException e){
+                e.printStackTrace();
+            }
+            System.out.println(i + " " + Thread.currentThread().getName());
+        }
     }
 }
 
